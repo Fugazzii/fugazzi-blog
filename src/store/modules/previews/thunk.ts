74a35ef0bf44d0594bdbcea4d318b8e5faa49e45
Fugazzii <sichinavailia@gmail.com>
2024-03-12 19:58:21 +0400

    Removed thunk

--- src/store/modules/previews/thunk.ts
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { previewsApi } from "./api";
-
-export const fetchPreviews = createAsyncThunk(
-    "previews/fetchPreviews",
-    () => previewsApi.endpoints.getAllPreviews.initiate("")
-);
