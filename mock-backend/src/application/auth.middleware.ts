600598bbc67a7c7a6576b85b3912f7f16ae5b1ff
Fugazzii <sichinavailia@gmail.com>
2024-05-16 13:27:05 +0400

    Changed url

--- mock-backend/src/application/auth.middleware.ts
+++ mock-backend/src/application/auth.middleware.ts
@@ -5,7 +5,7 @@ const config: ConfigParams = {
     authRequired: false,
     auth0Logout: true,
     secret: "3bc6f8bd71f7bd67870c96c97163c438134f17b58e2d66354a1bfe79b38ac972",
-    baseURL: "http://localhost:8080",
+    baseURL: "http://localhost:8888",
     clientID: "VihID3upBOkQIpuNqmWqmpTfjRwYSyB8",
     issuerBaseURL: "https://dev-6bi988f9.us.auth0.com",
     idpLogout: true
--- mock-backend/src/main.ts
+++ mock-backend/src/main.ts
@@ -5,7 +5,7 @@ const app = express();
 prepare(app)
     .then(() => {
         console.log("Application is ready");
-        const PORT = process.env.PORT || 8080;
+        const PORT = process.env.PORT || 8888;
         app.listen(PORT, () => {
             console.log(`Server is listening on ${PORT}`);
         });
